12afe3fe82aaf99faa684c17d2db3f3340992c21
qulad <ahmetertugrulkaya@gmail.com>
2024-01-11 23:55:01 +0300

    extender logic update

--- src/Extender.v
+++ src/Extender.v
@@ -1,6 +1,8 @@
 module Extender (
     input   wire    [5:0]   imm,
+    input   wire    [7:0]   addr,
     input   wire            control,
+    input   wire            beq,
 
     output  reg     [7:0]   extendedControl0,
     output  reg     [15:0]  extendedControl1
@@ -15,9 +17,16 @@ module Extender (
             extendedControl1 = temp15;
         end
         else begin
-            temp[7:2] = imm;
-        
-            extendedControl0 = temp;
+            if (beq) begin
+                temp15[15:8] = addr;
+
+                extendedControl1 = temp15;
+            end
+            else begin
+                temp15[7:2] = imm;
+
+                extendedControl0 = temp15;
+            end
         end
     end
     
