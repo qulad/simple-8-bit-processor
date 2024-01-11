b7c27a7276cbe8aed5802db7c87c3d319c00bae2
qulad <ahmetertugrulkaya@gmail.com>
2024-01-11 22:46:10 +0300

    fix mux

--- Extender.v
+++ Extender.v
@@ -1,14 +1,24 @@
 module Extender (
     input   wire    [5:0]   imm,
+    input   wire            control,
 
-    output  reg     [7:0]   extended
+    output  reg     [7:0]   extendedControl0,
+    output  reg     [15:0]  extendedControl1
 );
-    reg [7:0] temp;
+    reg [7:0] temp8;
+    reg [15:0] temp15;
 
     always @(*) begin
-        temp[7:2] = imm;
+        if (control) begin
+            temp15[15:10] = imm;
+
+            extendedControl1 = temp15;
+        end
+        else begin
+            temp[7:2] = imm;
         
-        extended = temp;
+            extendedControl0 = temp;
+        end
     end
     
 endmodule
