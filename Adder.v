b04dbc2c6e9638fd2ec8a67856ae0c820799aa5d
qulad <ahmetertugrulkaya@gmail.com>
2024-01-11 22:54:27 +0300

    adder

---
+++ Adder.v
@@ -0,0 +1,14 @@
+module Alu(
+    input   wire    [7:0]   input8,
+    input   wire    [15:0]  input16,
+
+    output  reg     [15:0]  result,
+);
+    reg [15:0]  temp;
+
+    always @(*) begin
+        temp[15:8] = input8;
+
+        result <= input16 + temp;
+    end
+endmodule
