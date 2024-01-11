ddc9e13e8a97d817261ce0622933a2f8d9cd767d
qulad <ahmetertugrulkaya@gmail.com>
2024-01-11 22:14:02 +0300

    extender

---
+++ Extender.v
@@ -0,0 +1,14 @@
+module Extender (
+    input wire [5:0] imm,
+
+    output reg [7:0] extended
+);
+    reg [7:0] temp;
+
+    always @(*) begin
+        temp[7:2] = imm;
+        
+        extended = temp;
+    end
+    
+endmodule
