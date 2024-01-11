643efbb1e7aacebf72b1e4589188eb75b4282fb7
qulad <ahmetertugrulkaya@gmail.com>
2024-01-11 22:15:46 +0300

    fix mux8

--- Mux8.v
+++ Mux8.v
@@ -1,9 +1,17 @@
 module Mux8 (
-    input wire [7:0] inputA,
-    input wire [7:0] inputB,
+    input wire [7:0] inputControl0,
+    input wire [7:0] inputControl1,
     input wire control,
 
     output reg [7:0] muxed
 );
+    always @(*) begin
+        if (control) begin
+            muxed = inputControl1;
+        end
+        else begin
+            muxed = inputControl0;
+        end
+    end
     
 endmodule
